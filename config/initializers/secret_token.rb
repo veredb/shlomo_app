65a42deddd776f6884da463a85aa581c93adf44c
vered bauer <veredbr2000@yahoo.com>
2013-03-05 11:57:44 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShlomoApp::Application.config.secret_token = 'f2a3dcbf1ecb688a1943af296833f223155d24678aba344594043d687673298972784ba03b06824349c9ef5fd201f72be23c4a711150b8163470eddedcdb00b3'
